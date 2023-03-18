99f4ee344e5c95f309c0ece031d0eaddd4e39592
cgarling <chris.t.garling@gmail.com>
2023-03-18 19:20:19 -0400

    removed old fit_template_mdf methods
    old methods used Optim.jl's fminbox and lbfgsb to do the optimization; worse than using Optim.jl with log transform

--- src/fitting.jl
+++ src/fitting.jl
@@ -605,51 +605,6 @@ This version of mdf fg! also fits σ
     end
 end
 
-# This version fits σ and so doesn't take that as a final argument
-# function fit_templates_mdf(models::AbstractVector{T},
-#                            data::AbstractMatrix{<:Number},
-#                            logAge::AbstractVector{<:Number},
-#                            metallicities::AbstractVector{<:Number};
-#                            composite=Matrix{S}(undef,size(data)),
-#                            x0=vcat(construct_x0_mdf(logAge), [-0.1, -0.5, 0.3]),
-#                            factr::Number=1e-12,
-#                            pgtol::Number=1e-5,
-#                            iprint::Integer=0,
-#                            kws...) where {S <: Number, T <: AbstractMatrix{S}}
-#     G = similar(x0)
-#     fg(x) = (R = SFH.fg_mdf!(true,G,x,models,data,composite,logAge,metallicities); return R,G)
-#     unique_logage = unique(logAge)
-#     @assert length(x0) == length(unique_logage)+3
-#     lb = vcat( zeros(length(unique_logage)), [-Inf, -Inf, 0.0])
-#     ub = fill(Inf,length(unique_logage)+3)
-#     LBFGSB.lbfgsb(fg, x0; lb=lb, ub=ub, factr=factr, pgtol=pgtol, iprint=iprint, kws...)
-# end
-
-# This performs worse than log-transformed version and the inverse Hessian seems
-# to result in less accurate uncertainty estimates so not certain this is better in any way.
-# function fit_templates_mdf_optim(models::AbstractVector{T},
-#                                  data::AbstractMatrix{<:Number},
-#                                  logAge::AbstractVector{<:Number},
-#                                  metallicities::AbstractVector{<:Number};
-#                                  composite=Matrix{S}(undef,size(data)),
-#                                  x0=vcat(construct_x0_mdf(logAge), [-0.1, -0.5, 0.3]),
-#                                  f_tol::Number=1e-10,
-#                                  g_tol::Number=1e-5,
-#                                  x_tol::Number=1.0,
-#                                  kws...) where {S <: Number, T <: AbstractMatrix{S}}
-#     unique_logage = unique(logAge)
-#     @assert length(x0) == length(unique_logage)+3
-#     lb = vcat( zeros(length(unique_logage)), [-Inf, -Inf, 0.0])
-#     ub = fill(Inf,length(unique_logage)+3)
-#     return Optim.optimize(Optim.only_fg!( (F,G,x)->SFH.fg_mdf!(F,G,x,models,data,composite,logAge,metallicities) ),
-#                           lb, ub, # Bounds constraints
-#                           x0,
-#                           Optim.Fminbox(Optim.BFGS()),
-#                           # Optim.Options(; f_tol=f_tol,g_tol=g_tol,x_tol=x_tol,allow_f_increases=true,kws...))
-#                           Optim.Options(; f_tol=f_tol,g_tol=g_tol,x_tol=x_tol,allow_f_increases=true,
-#                                         store_trace=true,extended_trace=true,kws...))
-# end
-
 function fit_templates_mdf(models::AbstractVector{T},
                            data::AbstractMatrix{<:Number},
                            logAge::AbstractVector{<:Number},
