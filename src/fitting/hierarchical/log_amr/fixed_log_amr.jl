66b01c8bcb5be9bd612915358d27451d52bb5a8e
cgarling <chris.t.garling@gmail.com>
2024-02-07 18:44:41 -0500

    separate calculation from `fixed_log_amr`
    Now the coefficients \alpha and \beta are calculated from two points by the `_calculate_αβ_logamr` method, which is called internally by the alternate call signature of `fixed_log_amr`.

--- src/fitting/hierarchical/log_amr/fixed_log_amr.jl
+++ src/fitting/hierarchical/log_amr/fixed_log_amr.jl
@@ -17,11 +17,11 @@ function fixed_log_amr(models::AbstractVector{<:AbstractMatrix{<:Number}},
     return fixed_amr(models, data, logAge, metallicities, relweights; kws...)
 end
 
-"""
-    fixed_log_amr(models, data, logAge, metallicities, constraint1, constraint2, σ; Z_func=Z_from_MH, kws...)
-Call signature that takes two fixed points `low_constraint` and `high_constraint` that define points that must lie on the logarithmic age-metallicity relation and calculates the paramters α and β for you. Format is ([M/H], age [Gyr]), i.e. `constraint1 = (-2.5,13.7)` for the first point at [M/H] = -2.5 at 13.7 Gyr lookback time and `constraint2 = (-0.8,0.0)` for the second point at [M/H] = -0.8 at present-day (0.0 Gyr lookback time). Metallicities in [M/H] iare converted to metal mass fractions Z via the provided callable keyword argument `Z_func` which defaults to [`StarFormationHistories.Z_from_MH`](@ref).
-"""
-function fixed_log_amr(models, data, logAge, metallicities, low_constraint, high_constraint, σ; Z_func=Z_from_MH, kws...)
+"
+    (α, β) = _calculate_αβ_logamr(low_constraint, high_constraint, Z_func=Z_from_MH)
+Calculates linear Z (log [M/H]) AMR slope α and intercept β from two points on the line given by the first two arguments; more info given in [`StarFormationHistories.fixed_log_amr`](@ref). 
+"
+function _calculate_αβ_logamr(low_constraint, high_constraint, Z_func=Z_from_MH)
     # Written so that order of the constraints doesn't actually matter...
     times = (last(low_constraint), last(high_constraint))
     δt = maximum(times) - minimum(times)
@@ -30,6 +30,14 @@ function fixed_log_amr(models, data, logAge, metallicities, low_constraint, high
     Z_max = Z_func(maximum(MH_constraints))
     α = -(Z_max - Z_min) / δt
     β = Z_func(maximum(MH_constraints)) - α * minimum(times)
-    # return α, β
+    return α, β
+end
+
+"""
+    fixed_log_amr(models, data, logAge, metallicities, constraint1, constraint2, σ; Z_func=Z_from_MH, kws...)
+Call signature that takes two fixed points `low_constraint` and `high_constraint` that define points that must lie on the logarithmic age-metallicity relation and calculates the paramters α and β for you. Format is ([M/H], age [Gyr]), i.e. `constraint1 = (-2.5,13.7)` for the first point at [M/H] = -2.5 at 13.7 Gyr lookback time and `constraint2 = (-0.8,0.0)` for the second point at [M/H] = -0.8 at present-day (0.0 Gyr lookback time). Metallicities in [M/H] iare converted to metal mass fractions Z via the provided callable keyword argument `Z_func` which defaults to [`StarFormationHistories.Z_from_MH`](@ref).
+"""
+function fixed_log_amr(models, data, logAge, metallicities, low_constraint, high_constraint, σ; Z_func=Z_from_MH, kws...)
+    α, β = _calculate_αβ_logamr(low_constraint, high_constraint, Z_func)
     return fixed_log_amr(models, data, logAge, metallicities, α, β, σ; kws...)
 end
