03a670ac275c56d949c3ab5464708b82ace8a22d
cgarling <chris.t.garling@gmail.com>
2024-12-17 23:48:37 -0500

    Functioning `LinearAMR` generic implementation
    `linear_amr_test_new.jl` covers all cases from previous `linear_amr_test.jl` and cases from `mzr_test.jl`. Since the linear AMR tests are fairly exhaustive WRT to the AMR API, we perhaps don't need as many tests for the logarithmic AMR model. Maybe just test things like calculate coeffs, make sure `fg!` runs, and so on?

--- src/fitting/hierarchical/hierarchical_models.jl
+++ src/fitting/hierarchical/hierarchical_models.jl
@@ -6,10 +6,11 @@ Base.Broadcast.broadcastable(m::AbstractMetallicityModel) = Ref(m)
 
 
 include("transformations.jl")   # Variable transformations
-include("dispersion_models.jl") # AbstractDispersionModel
+include("dispersion_models.jl") # AbstractDispersionModel and subtypes
 include("bfgs_result.jl")       # BFGSResult and CompositeBFGSResult types
 include("fixed_amr.jl")         # Fit under a fixed AMR -- deprecate?
 include("linear_amr/linear_amr.jl")
 include("log_amr/log_amr.jl")
+include("amr.jl")               # Age-metallicity relations
 include("mzr.jl")               # Mass-metallicity relations
 include("generic_fitting.jl")   # Fitting and sampling functions for both AMR and MZR
