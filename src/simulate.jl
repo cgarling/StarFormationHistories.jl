947ab92714f903818fd6132710968d2aa4dfac10
cgarling <chris.t.garling@gmail.com>
2025-07-08 13:12:20 -0400

    Implement photometric bias in `model_cmd`
    Implementation matches that of template construction methods.

--- src/simulate.jl
+++ src/simulate.jl
@@ -502,29 +502,31 @@ end
 """
     new_mags [, good_idxs] = model_cmd(mags::AbstractVector{<:AbstractVector{<:Number}}, 
                                        errfuncs, 
-                                       completefuncs; 
+                                       completefuncs,
+                                       biasfuncs = [zero for i in completefuncs]; 
                                        rng::Random.AbstractRNG = Random.default_rng(), 
                                        ret_idxs::Bool = false)
 
 Simple method for modeling photometric error and incompleteness to "mock observe" a pure catalog of stellar photometry, such as those produced by [`generate_stars_mass`](@ref) and [`generate_stars_mag`](@ref). This method assumes Gaussian photometric errors and that the photometric error and completeness functions are separable by filter. 
 
 # Arguments
- - `mags::AbstractVector{<:AbstractVector{<:Number}}`: a vector of vectors giving the magnitudes of each star to be modelled. The first index is the per-star index and the second index is the per-filter index (so `mags[10][2]` would give the magnitude of the tenth star in the second filter). This is the same format as the magnitudes returned by [`generate_stars_mass`](@ref) and [`generate_stars_mag`](@ref); to use output from the composite versions, you must first `reduce(vcat,mags)` before passing to this function.
+ - `mags::AbstractVector{<:AbstractVector{<:Number}}`: a vector of vectors giving the magnitudes of each star to be modeled. The first index is the per-star index and the second index is the per-filter index (so `mags[10][2]` would give the magnitude of the tenth star in the second filter). This is the same format as the magnitudes returned by [`generate_stars_mass`](@ref) and [`generate_stars_mag`](@ref); to use output from the composite versions, you must first `reduce(vcat,mags)` before passing to this function.
  - `errfuncs`: an iterable (typically a vector or tuple) of callables (typically functions or interpolators) with length equal to the number of filters contained in the elements of `mags`. This iterable must contain callables that, when called with the associated magnitudes from `mags`, will return the expected 1-σ photometric error at that magnitude. The organization is such that the photometric error for star `i` in band `j` is `σ_ij = errfuncs[j](mags[i][j])`. 
  - `completefuncs`: an iterable (typically a vector or tuple) of callables (typically functions or interpolators) with length equal to the number of filters contained in the elements of `mags`. This iterable must contain callables that, when called with the associated magnitudes from `mags`, will return the probability that a star with that magnitude in that band will be found in your color-magnitude diagram (this should include the original detection probability and any post-detection quality, morphology, or other cuts). The organization is such that the detection probability for star `i` in band `j` is `c_ij = completefuncs[j](mags[i][j])`.
+ - `biasfuncs = [zero for i in completefuncs]`: an iterable (typically a vector or tuple) of callables (typically functions or interpolators) with length equal to the number of filters contained in the elements of `mags`. This iterable must contain callables that, when called with the associated magnitudes from `mags`, will return the expectation value of the photometric bias at that magnitude. We define the photometric bias as the typical difference between measured and input magnitudes; i.e., `⟨measured - input⟩`. This definition matches that of the bias functions returned by [`process_ASTs`](@ref)). By default this method assumes no photometric bias.
 
 # Keyword Arguments
  - `rng::AbstractRNG = Random.default_rng()`: The object to use for random number generation.
  - `ret_idxs::Bool`: whether to return the indices of the input `mags` for the stars that were successfully "observed" and are represented in the output `new_mags`.
 
 # Returns
- - `new_mags`: an object similar to `mags` (i.e., a `Vector{Vector{<:Number}}`, `Vector{SVector{N,<:Number}}`, etc.) containing the magnitudes of the mock-observed stars. This will be shorter than the provided `mags` vector as we are modelling photometric incompleteness, and the magnitudes will also have random photometric errors added to them. This can be reinterpreted as a 2-dimensional `Matrix` with `reduce(hcat,new_mags)`.
+ - `new_mags`: an object similar to `mags` (i.e., a `Vector{Vector{<:Number}}`, `Vector{SVector{N,<:Number}}`, etc.) containing the magnitudes of the mock-observed stars. This will be shorter than the provided `mags` vector as we are modeling photometric incompleteness, and the magnitudes will also have random photometric errors added to them. This can be reinterpreted as a 2-dimensional `Matrix` with `reduce(hcat,new_mags)`.
  - `good_idxs`: if `ret_idxs` is `true`, the vector of indices into the input `mags` for the stars that were successfully "observed" and are represented in the output `new_mags`.
 
 # Notes
  - This is a simple implementation that seeks to show a simple example of how one can post-process catalogs of "pure" stars from methods like [`generate_stars_mass`](@ref) and [`generate_stars_mag`](@ref) to include observational effects. This method assumes Gaussian photometric errors, which may not, in general, be accurate. It also assumes that the total detection probability can be modeled as the product of the single-filter detection probabilities as computed by `completefuncs` (i.e., that the completeness functions are separable across filters). This can be a reasonable assumption when you have separate photometric catalogs derived for each filter and you only collate them afterwards, but it is generally not a good assumption for detection algorithms that operate simultaneously on multi-band photometry -- the completeness functions for these types of algorithms are generally not separable.
 """
-function model_cmd(mags::AbstractVector{T}, errfuncs, completefuncs;
+function model_cmd(mags::AbstractVector{T}, errfuncs, completefuncs, biasfuncs=[zero for i in completefuncs];
                    rng::AbstractRNG=default_rng(), ret_idxs::Bool=false) where T <: AbstractVector{<:Number}
     nstars = length(mags)
     nfilters = length(first(mags))
@@ -541,7 +543,7 @@ function model_cmd(mags::AbstractVector{T}, errfuncs, completefuncs;
     good = findall(map(<=, randsamp, completeness)) # findall( randsamp .<= completeness )
     ret_mags = mags[good] # Get the good mags to be returned.
     # Calculate and add photometric errors, see below
-    ret_mags = _model_cmd_errloop(rng, ret_mags, errfuncs)
+    ret_mags = _model_cmd_loop(rng, ret_mags, errfuncs, biasfuncs)
     if ret_idxs
         return ret_mags, good
     else
@@ -549,26 +551,31 @@ function model_cmd(mags::AbstractVector{T}, errfuncs, completefuncs;
     end
 end
 """
-    _model_cmd_errloop(rng, ret_mags::AbstractVector{T}, errfuncs) where {T <: AbstractVector{<:Number}}
-    _model_cmd_errloop(rng, ret_mags::AbstractVector{SVector{N, T}}, errfuncs) where {N, T <: Number}
+    _model_cmd_loop(rng, ret_mags::AbstractVector{T}, errfuncs, biasfuncs) where {T <: AbstractVector{<:Number}}
+    _model_cmd_loop(rng, ret_mags::AbstractVector{SVector{N, T}}, errfuncs, biasfuncs) where {N, T <: Number}
 
 Given a vector of vectors `ret_mags` that contains the magnitudes of stars that pass the completeness criterion,
-sample photometric errors from `errfuncs` and return a vector of vectors of the same size with photometric errors
-applied. This requires a different implementation for `StaticArrays.SVector` and regular `Vector`s, which is why
-we remove this process from `model_cmd`.
+sample photometric errors from `errfuncs` and photometric biases from `biasfuncs` and return a vector of vectors
+of the same size with photometric errors and biases applied. This requires a different implementation for 
+`StaticArrays.SVector` and regular `Vector`s, which is why we remove this process from `model_cmd`.
 """
-function _model_cmd_errloop(rng, ret_mags::AbstractVector{T}, errfuncs) where {T <: AbstractVector{<:Number}}
+function _model_cmd_loop(rng, ret_mags::AbstractVector{T}, errfuncs, biasfuncs) where {T <: AbstractVector{<:Number}}
+    # Since these are nested vectors, mutating the vectors contained in 
+    # ret_mags will mutate the original `mags` argument to `model_cmd`, which may be undesirable.
+    # We need to use a deepcopy to prevent this, though halves the performance.
+    ret_mags = deepcopy(ret_mags)
     for i in eachindex(ret_mags)
         for j in eachindex(errfuncs)
-            ret_mags[i][j] += (randn(rng) * errfuncs[j](ret_mags[i][j]))
+            ret_mags[i][j] += (randn(rng) * errfuncs[j](ret_mags[i][j])) + biasfuncs[j](ret_mags[i][j])
         end
     end
     return ret_mags
 end
-function _model_cmd_errloop(rng, ret_mags::AbstractVector{SVector{N, T}}, errfuncs) where {N, T <: Number}
+function _model_cmd_loop(rng, ret_mags::AbstractVector{SVector{N, T}}, errfuncs, biasfuncs) where {N, T <: Number}
     for i in eachindex(ret_mags)
         err_scale = sacollect(SVector{N, T}, errfuncs[j](ret_mags[i][j]) for j in eachindex(errfuncs))
-        ret_mags[i] = ret_mags[i] .+ (randn(rng, SVector{N, T}) .* err_scale)
+        bias = sacollect(SVector{N, T}, biasfuncs[j](ret_mags[i][j]) for j in eachindex(biasfuncs))
+        ret_mags[i] = ret_mags[i] .+ (randn(rng, SVector{N, T}) .* err_scale) .+ bias
     end
     return ret_mags
 end
